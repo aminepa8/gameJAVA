47cf96a88556d23215e5129321491858e30cfccd
aminep moulay <aminepa8@gmail.com>
2020-03-25 21:14:16 +0100

    Rewrite BoardFactory and LevelsBoard code

--- GameMemoryProjectDesignPatterns/src/GameBoard.java
+++ GameMemoryProjectDesignPatterns/src/GameBoard.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -9,7 +12,7 @@ import java.util.List;
 public abstract class GameBoard {
    
 	private List<Card> cards = new ArrayList<Card>();
-	
+	private Card FirstCardOfPair, SecondCardOfPair, WildCard;
     public List<Card> getCards() {
 		return cards;
 	}
@@ -17,7 +20,7 @@ public abstract class GameBoard {
 		this.cards = cards;
 	}
 	public GameBoard()  {
-        
+		
     }
     // filling the JPanel with cards
     public void fillBoardView(JPanel view){
@@ -31,8 +34,55 @@ public abstract class GameBoard {
             cards.remove(c);
         }
     }
-    
+    //Good Method Haha Avoid Repetiton or WhatEver LOL.....
+    public void BoardMaker(ActionListener event,ArrayList<ImageIcon> ImageList,boolean HasWilCard) {
+        int cardPair = 1;
+	       
+        for(int  i = 0;i < ImageList.size();i++){  
+            if(i == ImageList.size() -1 && HasWilCard ){
+                ImageIcon imgIcon = ImageList.get(i);
+                
+                WildCard = new Card(imgIcon);
+                WildCard.SetID(cardPair);
+                WildCard.addActionListener(event);
+                cards.add(WildCard);
+            }
+            else {
+            	ImageIcon imgIcon =  ImageList.get(i);
+                FirstCardOfPair = new Card(imgIcon);
+                FirstCardOfPair.SetID(cardPair);
+                FirstCardOfPair.addActionListener(event);
+                FirstCardOfPair.setPreferredSize(new Dimension(20,20));
+                cards.add(FirstCardOfPair);
+
+                SecondCardOfPair = new Card(imgIcon);
+                SecondCardOfPair.SetID(cardPair);
+                SecondCardOfPair.addActionListener(event);
+                SecondCardOfPair.setPreferredSize(new Dimension(20,20));
+                cards.add(SecondCardOfPair);
+
+                cardPair++;
+            }
+          //making sure that  the cards get shuffled well 
+            Collections.shuffle(cards);
+    	   
+        }
+    }
     public abstract Memory CreateMemory();
     
+    
+    public ArrayList<ImageIcon> getImageList(ClassLoader loader,String[] imgFiles) {
+    	loader = getClass().getClassLoader();
+    	ArrayList<ImageIcon> ImageIconList = new ArrayList<ImageIcon>();
+    	
+    	for(int i = 0 ; i < imgFiles.length; i++) {
+    		
+    		ImageIcon imgIcon = new ImageIcon(loader.getResource(imgFiles[i]));
+    		System.out.println(imgFiles[i]);
+    		ImageIconList.add(imgIcon);
+    	}
+    	
+    	return ImageIconList ;
+    }
     
 }
--- GameMemoryProjectDesignPatterns/src/GameBoardHard.java
+++ GameMemoryProjectDesignPatterns/src/GameBoardHard.java
@@ -11,48 +11,17 @@ public class GameBoardHard extends GameBoard{
 	private String imgFiles[] = {"1.png", "2.png", "3.png", "4.png",
             "5.png", "6.png", "7.png", "8.png", "9.png", "10.png",
             "11.png", "12.png", "smile.png"};
-    private List<Card> cards = new ArrayList<Card>();
+   
     private ClassLoader loader = getClass().getClassLoader();
-    private Card FirstCardOfPair, SecondCardOfPair, WildCard;
+   
     
     public GameBoardHard() {
     	
     }
 	 public GameBoardHard(ActionListener event)  {
 	        super();
-	        int cardPair = 1;
-	       
-	        for(int  i = 0;i < 13;i++){  
-	            if(i == 12){
-	                ImageIcon imgIcon = new ImageIcon(loader.getResource(imgFiles[i]));
-	                
-	                WildCard = new Card(imgIcon);
-	                WildCard.SetID(cardPair);
-	                WildCard.addActionListener(event);
-	                cards.add(WildCard);
-	            }
-	            else {
-	                ImageIcon imgIcon = new ImageIcon(loader.getResource(imgFiles[i]));
-	                FirstCardOfPair = new Card(imgIcon);
-	                FirstCardOfPair.SetID(cardPair);
-	                FirstCardOfPair.addActionListener(event);
-	                FirstCardOfPair.setPreferredSize(new Dimension(20,20));
-	                cards.add(FirstCardOfPair);
-
-	                SecondCardOfPair = new Card(imgIcon);
-	                SecondCardOfPair.SetID(cardPair);
-	                SecondCardOfPair.addActionListener(event);
-	                SecondCardOfPair.setPreferredSize(new Dimension(20,20));
-	                cards.add(SecondCardOfPair);
-
-	                cardPair++;
-	            }
-	        }
-	        
-	        //making sure that  the cards get shuffled well 
-	        Collections.shuffle(cards);
-	        Collections.shuffle(cards);
-	        super.setCards(cards);
+	        ArrayList<ImageIcon> ImageIconList = super.getImageList(loader, imgFiles);
+	        super.BoardMaker(event, ImageIconList, true);
 	    }
 	 
 	 public void fillBoardView(JPanel view ){
